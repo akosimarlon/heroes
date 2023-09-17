019c9f877650eb5698b113a7bfa15e76f27243f3
akosimarlon <mardioc24@gmail.com>
2023-09-18 00:01:38 +0800

    update

--- admin/includes/timestamp.php
+++ admin/includes/timestamp.php
@@ -2,7 +2,7 @@
     include('timezone.php');
 ?>  
     <p class="text-center">
-    <span style="font-size: 18px; font-weight: normal;">
+    <span style="font-size: 17px; font-weight: normal;">
         <!-- <i class="fas fa-clock fa-sm text-dark-50"></i>  -->
         <span><?php echo $timestamp = date("h:i:s"); ?></span>
         <span><?php echo $timestamp = date("A"); ?></span>
