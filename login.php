37a9e7f92a0dc0a0d140d50b764e5868df0bb313
akosimarlon <mardioc24@gmail.com>
2023-09-17 18:14:57 +0800

    update

--- login.php
+++ login.php
@@ -55,7 +55,7 @@ if(isset($_SESSION['auth'])){
                                     <div class="text-center">                                        
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-                                    <hr>
+                                    
                                     <form class="user" action="logincode.php" method="POST">                                    
                                         <div class="form-group">
                                             <input type="email" name="email" class="form-control form-control-user"
@@ -76,13 +76,13 @@ if(isset($_SESSION['auth'])){
                                         <button class="btn btn-primary btn-user btn-block" type="submit" name="btn_login">
                                             Login
                                         </button>
-                                        <hr>
+                                        <!-- <hr>
                                         <a href="" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
                                         <a href="" class="btn btn-facebook btn-user btn-block">
                                             <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
+                                        </a> -->
                                     </form>
                                     <hr>
                                     <div class="text-center">
