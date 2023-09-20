29aa0d7c958f0307b34091efd23b77c641e75f86
akosimarlon <mardioc24@gmail.com>
2023-09-20 22:02:01 +0800

    update

--- login.php
+++ login.php
@@ -41,23 +41,8 @@ if(isset($_SESSION['auth'])){
             height: 100vh;
         }
 
-        
-            .content {
-            /* background-color:rgba(255,255,255,.8); */
-            background-image: url('assets/img/HEROES COVER PAGE.jpg');
-            background-repeat: no-repeat;
-            background-size: cover;       
-            border-radius:.25em;
-            box-shadow:0 0 .25em rgba(0,0,0,.25);
-            box-sizing:border-box;
-            left:50%;
-            right:-30%;
-            padding:47vmin;
-            position:fixed;
-            text-align:center;
-            top:50%;        
-            transform:translate(-50%, -50%);
-            }
+        /* 
+            
 
             @keyframes gradient {
             0% {
@@ -69,8 +54,66 @@ if(isset($_SESSION['auth'])){
             100% {
                 background-position: 0% 50%;
             }
-        } 
-        
+        } */
+        html {
+        height:100%;
+        }
+
+        body {
+        margin:0;
+        }
+
+        .bg {
+        animation:slide 3s ease-in-out infinite alternate;
+        /* background-image: linear-gradient(-60deg, #f91 50%, #f43 50%); */
+        background-image: linear-gradient(-60deg, #6c3 50%, #09f 50%);
+        bottom:0;
+        left:-50%;
+        opacity:.5;
+        position:fixed;
+        right:-50%;
+        top:0;
+        z-index:-1;
+        }
+
+        .bg2 {
+        animation-direction:alternate-reverse;
+        animation-duration:4s;
+        }
+
+        .bg3 {
+        animation-duration:5s;
+        }
+
+        .content {
+        /* background-color:rgba(255,255,255,.8); */
+        background-image: url('assets/img/HEROES COVER PAGE.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;       
+        border-radius:.25em;
+        box-shadow:0 0 .25em rgba(0,0,0,.25);
+        box-sizing:border-box;
+        left:50%;
+        right:-30%;
+        padding:47vmin;
+        position:fixed;
+        text-align:center;
+        top:50%;        
+        transform:translate(-50%, -50%);
+        }
+
+        h1 {
+        font-family:monospace;
+        }
+
+        @keyframes slide {
+        0% {
+            transform:translateX(-25%);
+        }
+        100% {
+            transform:translateX(25%);
+        }
+        }
 
         /* @keyframes gradient {
             0% {
