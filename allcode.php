16d19be646fd7dcd3f1814fa21573b69da816f11
akosimarlon <mardioc24@gmail.com>
2023-09-22 00:33:59 +0800

    Update allcode.php

--- allcode.php
+++ allcode.php
@@ -5,16 +5,24 @@ include("admin/config/homeconfig.php");
 if(isset($_POST['btn_logout'])){
     unset( $_SESSION['auth'] );
     unset( $_SESSION['auth_role'] );
+    unset( $_SESSION['user_name'] );
+    unset( $_SESSION['user_empno'] );
     unset( $_SESSION['auth_user'] );
     unset( $_SESSION['tab_page'] );     
-    session_destroy();
+    unset( $_SESSION['message'] );     
+    unset( $_SESSION['message_type'] );     
+    //session_destroy();
 
-    $_SESSION['message'] = "Logged Out Successfully.";
-    $_SESSION['message_type'] = "primary";
+    //$_SESSION['message'] = "Logged Out Successfully.";
+    //$_SESSION['message_type'] = "primary";
     header("Location: ".$home_location);
     //header("Location: login.php"); //mao ni ang daan ug tinuod
     //header("Location: http://localhost:53548/"); //Home
     exit(0);
 }
 
+
+
+
 ?>
+
