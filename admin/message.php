908157f14ad0dcc7e58ae556fac99be46baa3e5f
akosimarlon <mardioc24@gmail.com>
2023-09-22 21:34:06 +0800

    Update message.php

--- admin/message.php
+++ admin/message.php
@@ -4,19 +4,19 @@ if(isset($_SESSION['message'])){
     <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
         <?php 
             if($_SESSION['message_type'] == "warning"){
-                ?><div class="toast align-items-center text-white bg-warning border-0" role="alert" aria-live="assertive" aria-atomic="true"><?php    
+                ?><div class="toast align-items-center text-white bg-warning border-0 hide" role="alert" aria-live="assertive" aria-atomic="true"><?php    
             }
             if($_SESSION['message_type'] == "success"){
-                ?><div class="toast align-items-center text-white bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true"><?php    
+                ?><div class="toast align-items-center text-white bg-success border-0 hide" role="alert" aria-live="assertive" aria-atomic="true"><?php    
             }
             if($_SESSION['message_type'] == "primary"){
-                ?><div class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true"><?php    
+                ?><div class="toast align-items-center text-white bg-primary border-0 hide" role="alert" aria-live="assertive" aria-atomic="true"><?php    
             }
             if($_SESSION['message_type'] == "danger"){
-                ?><div class="toast align-items-center text-white bg-danger border-0" role="alert" aria-live="assertive" aria-atomic="true"><?php    
+                ?><div class="toast align-items-center text-white bg-danger border-0 hide" role="alert" aria-live="assertive" aria-atomic="true"><?php    
             }
             if($_SESSION['message_type'] == "info"){
-                ?><div class="toast align-items-center text-white bg-info border-0" role="alert" aria-live="assertive" aria-atomic="true"><?php    
+                ?><div class="toast align-items-center text-white bg-info border-0 hide" role="alert" aria-live="assertive" aria-atomic="true"><?php    
             }
         ?>
             <div class="d-flex">
@@ -35,11 +35,11 @@ if(isset($_SESSION['message'])){
 
 <script>
 
-window.setTimeout(function() {
-    $(".alert").fadeTo(500, 0).slideUp(500, function(){
-        $(this).remove(); 
-    });
-}, 3000);
+// window.setTimeout(function() {
+//     $(".alert").fadeTo(500, 0).slideUp(500, function(){
+//         $(this).remove(); 
+//     });
+// }, 3000);
 
 </script>
 
