5815538a00a30dca527b6df313ac9a64c9109de1
akosimarlon <mardioc24@gmail.com>
2023-09-28 17:32:29 +0800

    Update register_teaching.php

--- admin/register_teaching.php
+++ admin/register_teaching.php
@@ -203,7 +203,7 @@
                 <div class="card-body">
 
                     <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
                             <thead>
                                 <tr>
                                     <!-- <th>ID</th> -->
