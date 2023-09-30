a367cbbfb8185290f62b99472963964a84d2d631
akosimarlon <mardioc24@gmail.com>
2023-09-30 21:53:52 +0800

    Update scripts.php

--- admin/includes/scripts.php
+++ admin/includes/scripts.php
@@ -17,6 +17,7 @@
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
     <script src="js/demo/chart-bar-demo.js"></script>
+    <script src="js/demo/chart-bar-demo non teaching.js"></script>
 
     <!-- Page level plugins -->
     <script src="vendor/datatables/jquery.dataTables.min.js"></script>
