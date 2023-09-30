436a64549a02ee15fd6928625eddee9a0eac634e
akosimarlon <mardioc24@gmail.com>
2023-09-30 21:44:47 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -152,7 +152,7 @@
                     </div>
                 </div>
                 
-                <!-- Content Row -->
+                <!-- Content Row Teaching Personnel-->
                 <div class="row">
                         <!-- Area Chart -->
                         <div class="col-xl-8 col-lg-7">
@@ -229,6 +229,86 @@
 
 
                 </div>
+
+
+                <!-- Content Row Non-Teaching Personnel-->
+                <div class="row">
+                        <!-- Area Chart -->
+                        <div class="col-xl-8 col-lg-7">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Teaching Personnel by Teaching Position</h6>
+                                    <div class="dropdown no-arrow">
+                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                                            aria-labelledby="dropdownMenuLink">
+                                            <div class="dropdown-header">Options:</div>
+                                            <a class="dropdown-item" href="reportEmployeePositions.php"><i class="fa fa-table"></i> Switch Tabular Data</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-bolt"></i> Another action</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-print"></i> Print Report</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    <div class="chart-bar">
+                                        <canvas id="myBarChart"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Pie Chart -->
+                        <div class="col-xl-4 col-lg-5">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Teaching Personnel by Gender</h6>
+                                    <div class="dropdown no-arrow">
+                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                                            aria-labelledby="dropdownMenuLink">
+                                            <div class="dropdown-header">Options:</div>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-table"></i> Switch Tabular Data</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-bolt"></i> Another action</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-print"></i> Print Report</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    <div class="chart-pie pt-4 pb-2">
+                                        <canvas id="myPieChart"></canvas>
+                                    </div>
+                                    <div class="mt-4 text-center small">
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-primary"></i> Male
+                                        </span>
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-success"></i> Female
+                                        </span>
+                                        <!-- <span class="mr-2">
+                                            <i class="fas fa-circle text-info"></i> Referral
+                                        </span> -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                </div>
+
                 <?php 
                     }
                 ?>
