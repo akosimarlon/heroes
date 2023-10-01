00f6774678c0682590e599606b8d18755e598ebc
akosimarlon <mardioc24@gmail.com>
2023-10-01 12:28:45 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -106,33 +106,49 @@
                         </a>
                     </div>
 
-                    <!-- Earnings (Monthly) Card Example -->
+                    <!-- Profile Completion -->
                     <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-left-info shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                                        </div>
-                                        <div class="row no-gutters align-items-center">
-                                            <div class="col-auto">
-                                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                        <a href="edit_teacherProfile.php?emp_no=<?=$_SESSION['auth_user']['user_empno']?>">                    
+                            <div class="card border-left-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Profile Details
                                             </div>
-                                            <div class="col">
-                                                <div class="progress progress-sm mr-2">
-                                                    <div class="progress-bar bg-info" role="progressbar"
-                                                        style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
+                                            <div class="row no-gutters align-items-center">
+                                                <?php
+                                                    $user_id = $_SESSION['user_empno'];
+                                                    $query = "SELECT * FROM profile_completion WHERE emp_no='$user_id' ";
+                                                    $query_run = mysqli_query($con,$query);
+
+                                                    if(mysqli_num_rows($query_run) > 0 ){ 
+                                                        foreach($query_run as $row){
+                                                    ?>                                                      
+                                                        <div class="col-auto">
+                                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?=$row['completed_percentage']?></div>
+                                                        </div>
+                                                        <div class="col">
+                                                            <div class="progress progress-sm mr-2">
+                                                                <div class="progress-bar progress-bar-striped bg-info progress-bar-animated" role="progressbar"
+                                                                    style="width: <?=$row['completed_percentage']?>" aria-valuenow="50" aria-valuemin="0"
+                                                                    aria-valuemax="100"></div>
+                                                            </div>
+                                                        </div>
+                                                    
+                                                <?php
+                                                        }
+                                                    }
+                                                ?> 
+                                                
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
 
                     <!-- Pending Requests Card Example -->
