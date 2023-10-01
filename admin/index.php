32c958a188c6cd7de8385c49f17dea5707f784ac
akosimarlon <mardioc24@gmail.com>
2023-10-01 12:33:17 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -110,7 +110,7 @@
                     <!-- Teaching Related Personnel -->
                     <div class="col-xl-3 col-md-6 mb-4">
                         <a href="">
-                            <div class="card border-left-success shadow h-100 py-2">
+                            <div class="card border-left-warning shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
