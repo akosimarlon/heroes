3a3e264b3f5c6fdec81dd24045392be017e01328
akosimarlon <mardioc24@gmail.com>
2023-10-15 14:30:55 +0800

    Update chart-pie-demo.js

--- admin/js/demo/chart-pie-demo.js
+++ admin/js/demo/chart-pie-demo.js
@@ -39,8 +39,8 @@ $.ajax({
         labels: gend,
         datasets: [{
           data: data_array2,
-          backgroundColor: ['#4e73df', '#1cc88a'], //, '#36b9cc'
-          hoverBackgroundColor: ['#2e59d9', '#17a673'], //, '#2c9faf'
+          backgroundColor: ['#7B68EE', '#FF69B4'], //, '#36b9cc'
+          hoverBackgroundColor: ['#6A5ACD', '#FF1493'], //, '#2c9faf'
           hoverBorderColor: "rgba(234, 236, 244, 1)",
         }],
       },
