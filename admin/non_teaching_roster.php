ce3e6177d531fd5fac254229aefe95c395df4fb7
akosimarlon <mardioc24@gmail.com>
2023-10-15 14:11:40 +0800

    Update non_teaching_roster.php

--- admin/non_teaching_roster.php
+++ admin/non_teaching_roster.php
@@ -139,7 +139,7 @@
                     <!-- Card Header - Dropdown -->
                     <div
                         class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Non Teaching Personnel by Teaching Position</h6>
+                        <h6 class="m-0 font-weight-bold text-primary">Non Teaching Personnel by Position</h6>
                         <div class="dropdown no-arrow">
                             <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
