20e29ba5bfb88385788fd07b226acf61c884b0d0
akosimarlon <mardioc24@gmail.com>
2023-10-28 21:06:02 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -22,7 +22,7 @@
         font-size: 41.5px;
     }
 
-    #cardTabs:Hover{
+    .card:Hover{
         box-shadow: 1px 1px 5px grey;
     }
 
@@ -56,7 +56,7 @@
                     <div class="col-xl-3 col-md-6 mb-4">
                         <a href="teaching_roster.php">
                             <div class="card bg-primary text-white shadow">
-                                <div class="card-body" id="cardTabs">
+                                <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-uppercase mb-1">
