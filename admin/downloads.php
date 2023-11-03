8962eaf1d08aeb3d3c7efe893c7149bb0cd10ade
akosimarlon <mardioc24@gmail.com>
2023-11-03 10:39:48 +0800

    Update downloads.php

--- admin/downloads.php
+++ admin/downloads.php
@@ -26,7 +26,7 @@
                         <option value="displayed">Displayed</option>
                         <option value="hidden">Hidden</option>
                     </select>
-
+                    <button type="button" class="btn btn-primary"><i class="fa fa-download"></i> Go</button>
                 </div>
             </div>
 
