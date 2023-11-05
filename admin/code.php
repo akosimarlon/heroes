1e3154118de94a772c84f3860d32180953f7964c
akosimarlon <mardioc24@gmail.com>
2023-11-05 19:00:44 +0800

    Update code.php

--- admin/code.php
+++ admin/code.php
@@ -3011,11 +3011,11 @@ if(isset($_POST['downloadCerts'])){
     // Important: You should have read and write permissions to read
     // the folder and write the zip file
     $zipArchive = new ZipArchive();
-    $zipFile = './example-zip-file.zip';
+    $zipFile = 'uploads/certificates/'.$lname.'_'.$fname.'_'.$empno.'/my_certificates.zip';
     if ($zipArchive->open($zipFile, ZipArchive::CREATE) !== TRUE) {
         exit("Unable to open file.");
     }
-    $folder = 'uploads/certificates/'. $lname.'_'.$fname.'_'.$empno.'/';
+    $folder = 'uploads/certificates/'.$lname.'_'.$fname.'_'.$empno.'/';
     createZip($zipArchive, $folder);
     $zipArchive->close();
 
@@ -3024,6 +3024,24 @@ if(isset($_POST['downloadCerts'])){
 
     //---------- Download -------------//
 
+    $filename = 'uploads/certificates/'.$lname.'_'.$fname.'_'.$empno.'/my_certificates.zip';
+    if (file_exists($filename)) {
+        // adjust the below absolute file path according to the folder you have downloaded
+        // the zip file
+        // I have downloaded the zip file to the current folder
+        $absoluteFilePath = __DIR__ . '/' . $filename;
+        header('Pragma: public');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Cache-Control: private', false);
+        // content-type has to be defined according to the file extension (filetype)
+        header('Content-Type: application/zip');
+        header('Content-Disposition: attachment; filename="' . basename($filename) . '";');
+        header('Content-Transfer-Encoding: binary');
+        header('Content-Length: ' . filesize($absoluteFilePath));
+        readfile($absoluteFilePath);
+        exit();
+    }
 
    
 }
