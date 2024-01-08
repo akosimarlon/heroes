7548e903bf140c7840cacf33b326c111291d1686
akosimarlon <mardioc24@gmail.com>
2024-01-08 20:10:39 +0800

    Update teaching_roster.php

--- admin/teaching_roster.php
+++ admin/teaching_roster.php
@@ -29,7 +29,7 @@
         <div class="modal fade" id="employmentInfoModal" tabindex="-1" aria-labelledby="employmentInfoModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
-                <div class="modal-header bg-primary">
+                <div class="modal-header bg-primary text-light">
                     <h1 class="modal-title fs-5" id="exampleModalLabel">Verify Employment Information</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
