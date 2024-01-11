d01ad3966170f113459d85b0d4a230e447ab3a4f
akosimarlon <mardioc24@gmail.com>
2024-01-11 21:13:42 +0800

    Update teaching_roster.php

--- admin/teaching_roster.php
+++ admin/teaching_roster.php
@@ -140,11 +140,13 @@
                 <div class="card-header py-3">
                     <div class="d-sm-flex align-items-center justify-content-between">
                         <h6 class="m-0 font-weight-bold text-primary">Teaching Personnel</h6>
-                        <div class="col">
-                            <span class="badge bg-primary">Approved: </span>
-                            <span class="badge bg-warning">Pending: </span>
-                            <span class="badge bg-danger">Disapproved: </span>
-                        </div>    
+                        <div class="row">
+                            <div class="col">
+                                <span class="badge bg-primary">Approved: </span>
+                                <span class="badge bg-warning">Pending: </span>
+                                <span class="badge bg-danger">Disapproved: </span>
+                            </div> 
+                        </div>   
                     </div>
                 </div>
                 <div class="card-body">
