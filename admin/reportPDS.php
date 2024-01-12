3663546ad2adfa2f39b319072ffb95cb285935e4
akosimarlon <mardioc24@gmail.com>
2024-01-12 11:27:33 +0800

    Update reportPDS.php

--- admin/reportPDS.php
+++ admin/reportPDS.php
@@ -1371,7 +1371,7 @@ class myPDF extends FPDF{
                     $this->SetFont('Arial','',4);
                 }
                 if($course_name > 98 ){
-                    $this->SetFont('Helvetica','',3);
+                    $this->SetFont('Helvetica','',2.5);
                 }
                 //$this->Cell(45,6,strtoupper($data->e_nameofschool),1,0,'C');
                 if($course_name < 40 ){
