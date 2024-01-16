d4b9cba9afd8098f7ba50166c89b302a97259685
akosimarlon <mardioc24@gmail.com>
2024-01-16 23:45:20 +0800

    Update edit_teacherProfile.php

--- admin/edit_teacherProfile.php
+++ admin/edit_teacherProfile.php
@@ -4498,7 +4498,7 @@
 
                                     <!-- <button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button> -->
 
-                                    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+                                    <!-- <div class="toast-container position-fixed bottom-0 end-0 p-3">
                                         <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
                                             <div class="toast-header">
                                             <img src="..." class="rounded me-2" alt="...">
@@ -4509,6 +4509,14 @@
                                             <div class="toast-body">
                                             Hello, world! This is a toast message.
                                             </div>
+                                        </div>
+                                    </div> -->
+                                    <div class="toast align-items-center" id="liveToast" role="alert" aria-live="assertive" aria-atomic="true">
+                                        <div class="d-flex">
+                                            <div class="toast-body">
+                                            Hello, world! This is a toast message.
+                                            </div>
+                                            <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
                                         </div>
                                     </div>
 
