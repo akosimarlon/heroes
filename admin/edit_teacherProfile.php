1d19110b986729ae71914e9f1b6448d307acce3d
akosimarlon <mardioc24@gmail.com>
2024-01-17 13:21:21 +0800

    Update edit_teacherProfile.php

--- admin/edit_teacherProfile.php
+++ admin/edit_teacherProfile.php
@@ -4511,12 +4511,14 @@
                                             </div>
                                         </div>
                                     </div> -->
-                                    <div class="toast align-items-center" id="liveToast" role="alert" aria-live="assertive" aria-atomic="true">
-                                        <div class="d-flex">
-                                            <div class="toast-body">
-                                            Hello, world! This is a toast message.
+                                    <div class="toast-container position-fixed top-0 end-0 p-3">
+                                        <div class="toast align-items-center" id="liveToast" role="alert" aria-live="assertive" aria-atomic="true">
+                                            <div class="d-flex">
+                                                <div class="toast-body">
+                                                Hello, world! This is a toast message.
+                                                </div>
+                                                <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
                                             </div>
-                                            <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
                                         </div>
                                     </div>
 
