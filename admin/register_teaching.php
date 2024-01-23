d179eb03d17500b0d343b4016f9923252484ff31
akosimarlon <mardioc24@gmail.com>
2024-01-23 19:39:07 +0800

    Update register_teaching.php

--- admin/register_teaching.php
+++ admin/register_teaching.php
@@ -1,16 +1,17 @@
 <?php
     //session_start();
-    include('authentication.php');
-    include('includes/header.php');
-    include('includes/navbar.php');  
-    unset( $_SESSION['tab_page'] );
-    
     if($_SESSION['auth_role'] != "1"){
         $_SESSION['message'] = "You are not an Authorized user to that page.";
         $_SESSION['message_type'] = "danger";
         header("Location: 403.php");
         exit(0);
     }
+    include('authentication.php');
+    include('includes/header.php');
+    include('includes/navbar.php');  
+    unset( $_SESSION['tab_page'] );
+    
+    
 ?>        
     <!-- Begin Page Content -->
     <div class="container-fluid">
