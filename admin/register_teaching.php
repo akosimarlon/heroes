7cfa70733ed5f7b24e8e642f4fd327cebba6b160
akosimarlon <mardioc24@gmail.com>
2024-01-23 19:57:18 +0800

    update

--- admin/register_teaching.php
+++ admin/register_teaching.php
@@ -17,8 +17,7 @@
     <div class="container-fluid">
 
         <!-- Page Heading -->
-        <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h3><?=$_SESSION['auth_role']?></h3>
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">        
             <h1 class="h3 mb-0 text-gray-800">User Accounts</h3> </h1>    
         </div>
 
