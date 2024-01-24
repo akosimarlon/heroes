7b6141ec24d7c49831e32b59b82f5c7fd70e4385
akosimarlon <mardioc24@gmail.com>
2024-01-24 20:38:17 +0800

    Update authentication.php

--- admin/authentication.php
+++ admin/authentication.php
@@ -125,12 +125,15 @@ else{
 
 if(isset($_GET['emp_no'])){
     //strcmp($_GET['emp_no'],"1202650") != 0 && 
-    if(strcmp($_GET['emp_no'],$_SESSION['auth_user']['user_empno']) != 0){        
-        $_SESSION['message'] = "That action is not authorized!";
-        $_SESSION['message_type'] = "danger";
-        header("Location: 403.php");
-        exit(0);
+    if(strcmp($_SESSION['auth_user']['user_empno'],"1202650") != 0){
+        if(strcmp($_GET['emp_no'],$_SESSION['auth_user']['user_empno']) != 0){        
+            $_SESSION['message'] = "That action is not authorized!";
+            $_SESSION['message_type'] = "danger";
+            header("Location: 403.php");
+            exit(0);
+        }
     }
+    
 }
 
 // if($_SESSION['user_empno'] != $_GET['emp_no'] ){
